f1c3e5f82dc877b57b811a0a1ba765a2c0c7d294
Aditya <adityanarayanabhat@gmail.com>
2025-03-08 22:02:02 +0530

    minor update to gemini service

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -8,17 +8,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 genai.configure(api_key=settings.GEMINI_API_KEY) # Configure Gemini API key
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 def generate_response_with_gemini_streaming(prompt: str):
     """Calls Google Gemini API and returns a streaming response."""
-    model = genai.GenerativeModel("gemini-2.0-flash")
 
     response_stream = model.generate_content(
         prompt,
         stream=True,
         generation_config={
             "temperature": 0.3,
-            "max_output_tokens": 1024,
+            "max_output_tokens": 3072,
             "response_mime_type": "text/plain"
         },
     )
